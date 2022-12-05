b67c1d867836793db61e3c5e11ac0561fd79a201
DevanShea2019 <devanshea2019@tamu.edu>
2022-12-05 10:06:48 -0600

    Implement Seasonal Item Addition
    Also minor routing changes

---
+++ src/ServerCheckout.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ServerDishChoiceCurrentOrder from "./ServerDishChoiceCurrentOrder";
+import '../index2.css';
+import ServerDishChoiceButton from './ServerDishChoiceButton';
+import { useNavigate } from 'react-router-dom';
+import TotalPrice from './TotalPrice';
+import ServerCheckoutButton from './ServerCheckoutButton';
+import PandaLogo from './panda-logo.png';
+
+
+const ServerCheckout = () => {
+
+  let navigate = useNavigate();
+  return (
+    <div class = "CheckoutScreen">
+        <img class = "GoHome" id = "gohomeCheckout" src={PandaLogo} onClick={() => {navigate("/ServerMenu")}}/>
+        <div class = "ServerCheckoutOrder"><ServerDishChoiceCurrentOrder /></div>
+        <div id = "ServerCheckout"><ServerCheckoutButton /></div>
+        <div class = "ServerCheckoutButton" id = "ServerGoBack" onClick={() => {navigate("/ServerMenu")}}><ServerDishChoiceButton Name = "Go Back" /></div>
+        <div id = "ServerPrice" onClick={() => {window.location.reload()}}><TotalPrice /></div>
+    </div>
+    )
+}
+
+export default ServerCheckout
