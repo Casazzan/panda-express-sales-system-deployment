9efb3e3b9c123891be5bb14e9cf9381e12709d84
Anna Slater <annakate.slater@gmail.com>
2022-12-05 16:53:37 -0600

    added translation for order history

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -13,7 +13,7 @@ function Accessibility() {
 
   window.addEventListener('storage', (e) => {
       console.log("change to local storage!");
-      changeText("Submit")
+      changeText()
   });
   
   const getLanguages = () => {
@@ -39,36 +39,55 @@ function Accessibility() {
     });
   }
 
-  const changeText = (input) => {
-  // var selected = document.getElementById("selectedLanguageDiv").innerHTML;
-  if (JSON.parse(localStorage.getItem("language")) != "en") {
-    const encodedParams = new URLSearchParams();
-    encodedParams.append("q", input);
-    encodedParams.append("target", JSON.parse(localStorage.getItem("language")));
-    encodedParams.append("source", "en");
+  const changeText = () => {
+    // var selected = document.getElementById("selectedLanguageDiv").innerHTML;
+    if (localStorage.getItem("language") == null) {
+      localStorage.setItem("language", JSON.stringify("en"));
+    }
+    console.log(localStorage.getItem("language"))
+    if (localStorage.getItem("language") != "en") {
+      const encodedParams = new URLSearchParams();
+      encodedParams.append("q", "Submit");
+      encodedParams.append("q", "Home");
+      encodedParams.append("q", "Sales");
+      encodedParams.append("q", "Employee");
+      encodedParams.append("q", "Inventory");
+      encodedParams.append("q", "Accessibility");
+      encodedParams.append("target", JSON.parse(localStorage.getItem("language")));
+      encodedParams.append("source", "en");
 
-    const options = {
-      method: 'POST',
-      url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'Accept-Encoding': 'application/gzip',
-        'X-RapidAPI-Key': '8fbc873fd8msh5d43e6022f22f64p15f17ejsnbb456384ba17',
-        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-      },
-      data: encodedParams
-    };
+      const options = {
+        method: 'POST',
+        url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          'Accept-Encoding': 'application/gzip',
+          'X-RapidAPI-Key': '8fbc873fd8msh5d43e6022f22f64p15f17ejsnbb456384ba17',
+          'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+        },
+        data: encodedParams
+      };
 
-    axios.request(options).then(function (response) {
-        console.log(response.data);
-        setTranslatedText(response.data.data.translations[0].translatedText);
-    }).catch(function (error) {
-        console.error(error);
-    });
-  }
-  else {
-    setTranslatedText(input)
-  }
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+          setTranslatedText(response.data.data.translations[0].translatedText);
+          localStorage.setItem("home", JSON.stringify(response.data.data.translations[1].translatedText));
+          localStorage.setItem("sales", JSON.stringify(response.data.data.translations[2].translatedText));
+          localStorage.setItem("employee", JSON.stringify(response.data.data.translations[3].translatedText));
+          localStorage.setItem("inventory", JSON.stringify(response.data.data.translations[4].translatedText));
+          localStorage.setItem("accessibility", JSON.stringify(response.data.data.translations[5].translatedText));
+      }).catch(function (error) {
+          console.error(error);
+      });
+    }
+    else {
+      setTranslatedText("Submit")
+      localStorage.setItem("home", JSON.stringify("Home"));
+      localStorage.setItem("sales", JSON.stringify("Sales"));
+      localStorage.setItem("employee", JSON.stringify("Employee"));
+      localStorage.setItem("inventory", JSON.stringify("Inventory"));
+      localStorage.setItem("accessibility", JSON.stringify("Accessibility"));
+    }
 }
 
 
@@ -77,7 +96,16 @@ function Accessibility() {
   useEffect(() => {
     console.log("use effect here")
     getLanguages()
-    changeText("Submit")
+    if (localStorage.getItem("language") == null) {
+      localStorage.setItem("language", JSON.stringify("en"));
+      console.log("made it to change text")
+      changeText()
+      window.location.reload(false)
+    }
+    else {
+      changeText()
+    }
+
   }, [])
 
   const changeLanguage = () => {
